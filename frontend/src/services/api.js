236f73918dd44c9e42ded955b838b6aebf94a33b
HritikKumar89 <125345693+HritikKumar89@users.noreply.github.com>
2026-02-22 16:18:49 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,7 +4,7 @@ const API = axios.create({
   baseURL: "https://savra-technical-assignment-1.onrender.com/api/insights/summary"
 });
 
-export const getSummary = () => API.get("/summary");
+export const getSummary = () => API.get("/");
 
 export const getWeekly = (teacherId) =>
   API.get(`/weekly${teacherId ? `?teacherId=${teacherId}` : ""}`);
